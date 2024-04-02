920d4c213fc25471c4e5c98c69faab4fe8dea330
GuitarHero2004 <anhminhnts2004@gmail.com>
2024-04-02 23:46:17 +0700

    Implement the randomId generator for new Customer and new InsuranceCard

--- src/main/java/com/guitarhero2004/icms/generator/IdGenerator.java
+++ src/main/java/com/guitarhero2004/icms/generator/IdGenerator.java
@@ -3,16 +3,39 @@ package com.guitarhero2004.icms.generator;
 import java.util.Random;
 
 public class IdGenerator {
-    public static String generateRandomId (int length) {
-        String idDigits = "0000000001";
-        StringBuilder idBuilder = new StringBuilder();
+    public static String generateCustomerId () {
+        String prefix = "c-";
+        int length = 7;
+        String customerIdDigits = "0000001";
+        StringBuilder customerIdBuilder = new StringBuilder();
 
-        Random randomId = new Random();
+        Random randomCustomerId = new Random();
+
+        customerIdBuilder.append(prefix);
 
         for (int i = 0; i < length; i++) {
-            int index = randomId.nextInt(idDigits.length());
-            idBuilder.append(idDigits.charAt(index));
+            int index = randomCustomerId.nextInt(customerIdDigits.length());
+            customerIdBuilder.append(customerIdDigits.charAt(index));
         }
-        return idBuilder.toString();
+        return customerIdBuilder.toString();
     }
+
+
+    public static String generateInsuranceCardId () {
+        String prefix = "f-";
+        int length = 10;
+        String insuranceCardIdDigits = "0000000001";
+        StringBuilder insuranceCardIdBuilder = new StringBuilder();
+
+        Random randomInsuranceCardId = new Random();
+
+        insuranceCardIdBuilder.append(prefix);
+
+        for (int i = 0; i < length; i++) {
+            int index = randomInsuranceCardId.nextInt(insuranceCardIdDigits.length());
+            insuranceCardIdBuilder.append(insuranceCardIdDigits.charAt(index));
+        }
+        return insuranceCardIdBuilder.toString();
+    }
+
 }
